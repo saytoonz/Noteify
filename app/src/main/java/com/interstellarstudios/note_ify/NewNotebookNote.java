8da5cb94c3146e5005d92f111116ce6eb9dd272f
Craig Spicer <craigspicer@live.co.za>
2019-05-10 21:45:15 +0200

    Update NewNotebookNote.java

--- app/src/main/java/com/interstellarstudios/note_ify/NewNotebookNote.java
+++ app/src/main/java/com/interstellarstudios/note_ify/NewNotebookNote.java
@@ -986,7 +986,7 @@ public class NewNotebookNote extends AppCompatActivity implements DatePickerDial
                     ApiClient defaultClient = Configuration.getDefaultApiClient();
 
                     ApiKeyAuth apiKey = (ApiKeyAuth) defaultClient.getAuthentication("api-key");
-                    apiKey.setApiKey("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");
+                    apiKey.setApiKey("API KEY GOES HERE");
 
                     SmtpApi apiInstance = new SmtpApi();
 
