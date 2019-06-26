df8e8f11e635f74d9a0f4cb15cd86cedbbaf7ee2
Craig Spicer <craigspicer@live.co.za>
2019-06-26 20:39:09 +0200

    Update

--- app/src/main/java/com/interstellarstudios/note_ify/SendMail.java
+++ app/src/main/java/com/interstellarstudios/note_ify/SendMail.java
@@ -32,7 +32,7 @@ public class SendMail {
                     SendSmtpEmail sendSmtpEmail = new SendSmtpEmail();
                     sendSmtpEmail.sender(new SendSmtpEmailSender().email("note-ify@interstellarstudios.co.uk").name("Note-ify"));
                     sendSmtpEmail.to(emailArrayList);
-                    sendSmtpEmail.subject("You've received a note from " + currentUserEmail);
+                    sendSmtpEmail.subject("You've received a Note from " + currentUserEmail);
                     sendSmtpEmail.htmlContent("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\"><head><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" /><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /><meta name=\"x-apple-disable-message-reformatting\" /><meta name=\"apple-mobile-web-app-capable\" content=\"yes\" /><meta name=\"apple-mobile-web-app-status-bar-style\" content=\"black\" /><meta name=\"format-detection\" content=\"telephone=no\" /><title></title><style type=\"text/css\">\n" +
                             "        /* Resets */\n" +
                             "        .ReadMsgBody { width: 100%; background-color: #ebebeb;}\n" +
